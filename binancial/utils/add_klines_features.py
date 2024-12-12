694397ec40cfebd4d116d071074e960874551297
Mikko Kotila <mailme@mikkokotila.com>
2024-12-12 15:29:38 +0200

    feat: Add more trend and momentum features

--- binancial/utils/add_klines_features.py
+++ binancial/utils/add_klines_features.py
@@ -7,7 +7,7 @@ def add_klines_features(df):
     
     import wrangle as wr
 
-    from ..features.trend_count import trend_count
+    from ..features.trend_and_momentun import trend_and_momentun
     from ..features.is_day_highlow import is_day_highlow
     from ..features.price_changes_related import price_changes_related
     from ..features.datetime_related import datetime_related
