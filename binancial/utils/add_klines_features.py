5d48144bd45742b63e225c765d271617eb7cdb79
Mikko Kotila <mailme@mikkokotila.com>
2025-03-28 16:17:53 -0400

    fix: Update trend and momentum appends

--- binancial/utils/add_klines_features.py
+++ binancial/utils/add_klines_features.py
@@ -20,7 +20,7 @@ def add_klines_features(df):
     df = price_changes_related(df)
 
     # number of samples rising or falling trend has continued
-    df = trend_count(df)
+    df = trend_and_momentum(df)
 
     # if the sample is day highest or lowest
     df = is_day_highlow(df)
